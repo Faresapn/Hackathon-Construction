265c6a1853ffe871ad5a6a97d0b009f8d93336dd
Qoolqas <qoolqas@gmail.com>
2019-11-17 06:11:38 +0700

    rahman

--- app/src/main/java/com/example/digitalconstruction/registrasi/Register2.java
+++ app/src/main/java/com/example/digitalconstruction/registrasi/Register2.java
@@ -2,11 +2,10 @@ package com.example.digitalconstruction.registrasi;
 
 import android.os.Bundle;
 
-<<<<<<< HEAD:app/src/main/java/com/example/digitalconstruction/Register2.java
+
 import androidx.appcompat.app.AppCompatActivity;
-=======
+
 import com.example.digitalconstruction.R;
->>>>>>> b414288e4bc7a4a6ba2502b72248c7fe5ae793a2:app/src/main/java/com/example/digitalconstruction/registrasi/Register2.java
 
 public class Register2 extends AppCompatActivity {
 

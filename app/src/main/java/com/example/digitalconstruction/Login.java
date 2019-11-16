530dfd4f4329577a3ab2c53757cf39816634f03a
faresa prasetyo nugroho <faresaprasetyon@gmail.com>
2019-11-16 14:17:24 +0700

    faresa

---
+++ app/src/main/java/com/example/digitalconstruction/Login.java
@@ -0,0 +1,16 @@
+package com.example.digitalconstruction;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}

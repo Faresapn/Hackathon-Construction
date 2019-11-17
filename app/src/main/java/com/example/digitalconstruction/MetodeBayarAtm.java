9d737d928f41be2cf650118b43269290cb25b80a
faresa prasetyo nugroho <faresaprasetyon@gmail.com>
2019-11-17 07:36:25 +0700

    second

---
+++ app/src/main/java/com/example/digitalconstruction/MetodeBayarAtm.java
@@ -0,0 +1,14 @@
+package com.example.digitalconstruction;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MetodeBayarAtm extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_metode_bayar_atm);
+    }
+}

b56713e10287dee752bc8e86d88b36485c426254
ganargatul <ganar.gatul@gmail.com>
2019-11-17 12:44:42 +0700

    fix payment, but please fix on Detail_Rent xml

--- app/src/main/java/com/example/digitalconstruction/MetodeBayarAtm.java
+++ app/src/main/java/com/example/digitalconstruction/MetodeBayarAtm.java
@@ -1,14 +1,50 @@
 package com.example.digitalconstruction;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MetodeBayarAtm extends AppCompatActivity {
-
+    CardView bni,bca,mandiri,bri;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_metode_bayar_atm);
+        bni = findViewById(R.id.bni);
+        bni.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                intentt();
+            }
+        });
+        bca = findViewById(R.id.bca);
+        bca.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                intentt();
+            }
+        });
+        mandiri = findViewById(R.id.mandiri);
+        mandiri.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                intentt();
+            }
+        });
+        bri = findViewById(R.id.bri);
+        bri.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                intentt();
+            }
+        });
+    }
+
+    void intentt(){
+        startActivity(new Intent(MetodeBayarAtm.this,Pembayaran.class));
+
     }
 }
--- app/src/main/java/com/example/digitalconstruction/MetodePembayaran.java
+++ app/src/main/java/com/example/digitalconstruction/MetodePembayaran.java
@@ -2,13 +2,31 @@ package com.example.digitalconstruction;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 public class MetodePembayaran extends AppCompatActivity {
-
+    LinearLayout atm,bayarlangsung;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_metode_pembayaran);
+        atm = findViewById(R.id.atm);
+        atm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MetodePembayaran.this,MetodeBayarAtm.class));
+            }
+        });
+        bayarlangsung = findViewById(R.id.bayarlangsung);
+        bayarlangsung.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MetodePembayaran.this, "Under Development", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
